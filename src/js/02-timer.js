2baaebb1b289a64a9641de40a434a9be05f28b53
Veronika Kovalenko <podaritebegemotika@gmail.com>
2022-08-29 10:42:23 +0300

    і

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,124 +1,74 @@
-import Notiflix from 'notiflix';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
-const MESSAGE = 'Please choose a date in the future';
-const daysRef = document.querySelector('span[data-days]');
-const hoursRef = document.querySelector('span[data-hours]');
-const minutesRef = document.querySelector('span[data-minutes]');
-const secondsRef = document.querySelector('span[data-seconds]');
-const startBtn = document.querySelector('button[data-start]');
-const datePicker = document.querySelector('#datetime-picker');
+const text = document.querySelector('#datetime-picker');
+const timerHtml = document.querySelector('.timer');
+const btnStart = document.querySelector('button[data-start]');
+const seconds = document.querySelector('span[data-seconds]');
+const minutes = document.querySelector('span[data-minutes]');
+const hours = document.querySelector('span[data-hours]');
+const days = document.querySelector('span[data-days]');
 
-disableBtn();
-startBtn.addEventListener('click', () => {
-    timer.start();
-});
-
-function enableDatePicker() {
-    datePicker.removeAttribute('disabled');
-}
-
-function disableDatePicker() {
-    datePicker.setAttribute('disabled', 'true');
-}
-
-function enableBtn() {
-    startBtn.removeAttribute('disabled');
-}
-
-function disableBtn() {
-    startBtn.setAttribute('disabled', 'true');
-}
-
-function resetTimer() {
-    daysRef.textContent = '00';
-    hoursRef.textContent = '00';
-    secondsRef.textContent = '00';
-    minutesRef.textContent = '00';
-}
-
-let dateX;
+btnStart.disabled = true;
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        console.log(selectedDates[0]);
-        if (selectedDates[0] < Date.now()) {
-            disableBtn();
-            Notiflix.Notify.failure(MESSAGE, {
-                position: 'left-top',
-                width: '50vw',
-                clickToClose: true,
-            });
-        } else {
-            enableBtn();
-            dateX = selectedDates[0];
-        }
-    },
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      btnStart.disabled = true;
+    } else {
+      btnStart.disabled = false;
+    }
+  },
 };
 
-console.log(new Date());
-const fp = flatpickr('#datetime-picker', options);
+flatpickr(text, options);
+
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    // Remaining days
-    const days = addLeadingZero(Math.floor(ms / day));
-    // Remaining hours
-    const hours = addLeadingZero(Math.floor((ms % day) / hour));
-    // Remaining minutes
-    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-    // Remaining seconds
-    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-    return { days, hours, minutes, seconds };
+  return { days, hours, minutes, seconds };
 }
+
 function addLeadingZero(value) {
-    return String(value).padStart(2, '0');
+  return value.toString().padStart(2, '0');
 }
 
-const timer = {
-    intervalId: null,
-    start() {
-        disableBtn();
-        disableDatePicker();
-        const startTime = dateX.getTime();
-        this.intervalId = setInterval(() => {
-            const currentTime = Date.now();
-            const deltaTime = startTime - currentTime;
-            const { days, hours, minutes, seconds } = convertMs(deltaTime);
-            daysRef.textContent = `${days}`;
-            hoursRef.textContent = `${hours}`;
-            minutesRef.textContent = `${minutes}`;
-            secondsRef.textContent = `${seconds}`;
-            if (Number(secondsRef.textContent) < 20) {
-                secondsRef.classList.add('danger');
-            }
-            if (deltaTime <= 0) {
-                this.stop();
-                resetTimer();
-                disableBtn();
-            }
-        }, 1000);
-    },
-    stop() {
-        clearInterval(this.intervalId);
-        enableDatePicker();
-        secondsRef.classList.remove('danger');
-        Notiflix.Notify.failure('TIME IS OUT!', {
-            position: 'center-center',
-            width: '100vw',
-            clickToClose: true,
-            fontSize: '4em',
-        });
-    },
-};
+btnStart.addEventListener('click', () => {
+  let timer = setInterval(() => {
+    let countdown = new Date(text.value) - new Date();
+    btnStart.disabled = true;
+    if (countdown >= 0) {
+      let timeObject = convertMs(countdown);
+      days.textContent = addLeadingZero(timeObject.days);
+      hours.textContent = addLeadingZero(timeObject.hours);
+      minutes.textContent = addLeadingZero(timeObject.minutes);
+      seconds.textContent = addLeadingZero(timeObject.seconds);
+      if (countdown <= 10000) {
+        timerHtml.style.color = 'tomato';
+      }
+    } else {
+      Notiflix.Notify.success('Countdown finished');
+      timerHtml.style.color = 'black';
+      clearInterval(timer);
+    }
+  }, 1000);
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,43 +1,38 @@
 import Notiflix from 'notiflix';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-const form = document.querySelector('.form');
-// const button = document.querySelector('button[type="submit"]');
-form.addEventListener('submit', onSubmit);
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const btnCreatePromise = document.querySelector('button[type="submit"]');
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  return new Promise((resolve, reject) => {
+  const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
-        // Fulfill
         resolve({ position, delay });
       } else {
-        // Reject
         reject({ position, delay });
       }
     }, delay);
   });
+  return promise;
 }
 
-function onSubmit(e) {
+btnCreatePromise.addEventListener('click', e => {
   e.preventDefault();
-  let delay = Number(form.elements.delay.value);
-  let step = Number(form.elements.step.value);
-  let amount = form.elements.amount.value;
-  let position = 1;
-  for (let i = 1; i <= amount; i += 1) {
-    createPromise(position, delay)
+  let firstDelay = Number(delay.value);
+  let delayStep = Number(step.value);
+  for (let i = 0; i < amount.value; i++) {
+    createPromise(1 + i, firstDelay + i * delayStep)
       .then(({ position, delay }) => {
-        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
-          width: '40vw',
-        });
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
       })
       .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
-          width: '40vw',
-        });
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
       });
-    delay += step;
-    position += 1;
   }
-}
+});
